757e1df7fed751ef8ae07a30c8d5e94f5cc63348
Dashasoham <dikrain8@gmail.com>
2022-10-19 22:24:53 +0100

    finished skeleton

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <h1>Weather App </h1>
         <Weather />
       </header>
+      <footer>Coded by Daria Korniienko</footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Weather.css';
+import axios from 'axios';
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <form className="form-control">
-        <input type="Search" />
-        <input type="submit" className="btn btn-primary" />{' '}
+      <form className="m-4 pb-5">
+        {' '}
+        <div className="row">
+          {' '}
+          <div className="col-9">
+            <input
+              type="Search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" className="btn btn-primary w-100" />{' '}
+          </div>
+        </div>
       </form>
+
       <div className="container">
         <div class="row">
           <div class="col-2">
@@ -19,16 +34,16 @@ export default function Weather() {
           <div class="col-2">
             {' '}
             <span className="degrees">18</span>
-            <span>°C</span>
+            <span className="units">°C</span>
           </div>
-          <div class="col-2">
+          <div class="col-3">
             <ul>
               <li>Precipitation: 20%</li>
               <li>Humidity: 88%</li>
               <li>Wind: 24 km/h</li>
             </ul>
           </div>
-          <div class="col-3"></div>
+          <div class="col-2"></div>
           <div class="col-3">
             <div>Wednesday,19</div>
             <div> Mostly cloudy</div>
