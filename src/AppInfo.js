982def35951c185271ae8fde3ac80f8ea360eb1e
Dashasoham <dikrain8@gmail.com>
2022-10-20 21:55:49 +0100

    Ready without forecast

---
+++ src/AppInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import CurrentDate from './CurrentDate';
+
+export default function AppInfo(props) {
+  return (
+    <div className="container">
+      <h2 className="cityDisplay">{props.data.city}</h2>
+      <div className="row">
+        <div className=" icon col-2">
+          <img src={props.data.icon} alt="weather-icon" />
+        </div>
+        <div className="col-2">
+          {' '}
+          <span className="degrees">{Math.round(props.data.temperature)}</span>
+          <span className="units">°C</span>
+        </div>
+        <div className="col-3">
+          <ul>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-3">
+          <div className="date">
+            <CurrentDate date={props.data.date} />
+          </div>
+          <div className="description text-capitalize">
+            {' '}
+            {props.data.description}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Weather.css';
+import AppInfo from './AppInfo';
 import axios from 'axios';
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="m-4 pb-5">
-        {' '}
-        <div className="row">
-          {' '}
-          <div className="col-9">
-            <input
-              type="Search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" className="btn btn-primary w-100" />{' '}
-          </div>
-        </div>
-      </form>
+  let [weather, setWeather] = useState(false);
+  let [loaded, setLoaded] = useState(false);
+  let [city, setCity] = useState(null);
+  let [message, setMessage] = useState(null);
 
-      <div className="container">
-        <div class="row">
-          <div class="col-2">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Weather-icon"
-            />
-          </div>
-          <div class="col-2">
-            {' '}
-            <span className="degrees">18</span>
-            <span className="units">°C</span>
-          </div>
-          <div class="col-3">
-            <ul>
-              <li>Precipitation: 20%</li>
-              <li>Humidity: 88%</li>
-              <li>Wind: 24 km/h</li>
-            </ul>
-          </div>
-          <div class="col-2"></div>
-          <div class="col-3">
-            <div>Wednesday,19</div>
-            <div> Mostly cloudy</div>
-          </div>
+  function handleCity(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      date: new Date(response.data.dt * 1000),
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      city: response.data.name,
+    });
+    setMessage(city);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiKey = '7d2f7439094688bc9a2723b3273f8711';
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleCity);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className=" mt-4 pb-4" onSubmit={handleSubmit}>
+      {' '}
+      <div className="row">
+        {' '}
+        <div className="col-9">
+          <input
+            type="Search"
+            placeholder="Enter a city.."
+            className="form-control"
+            autoFocus="on"
+            onChange={updateCity}
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />{' '}
         </div>
       </div>
-    </div>
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <AppInfo data={weather} />
+      </div>
+    );
+  } else {
+    return <h1 className="Weather">{form}</h1>;
+  }
 }
