1eed144bddd15699abdc9bb5fb33610c99810dbc
Dashasoham <dikrain8@gmail.com>
2022-10-25 19:12:56 +0100

    working forecast API

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
+import ForecastDay from './ForecastDay';
 import axios from 'axios';
 
 export default function Forecast(props) {
   const [loaded, setLoaded] = useState(false);
-  const [test, setTest] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
 
   function handleForecast(response) {
+    setForecastData(response.data.daily);
     setLoaded(true);
-
-    setTest(response.data);
   }
 
   if (loaded) {
+    console.log(forecastData);
     return (
       <div className="container">
         <div className="raw">
-          <div className="col">Hi</div>
+          <ForecastDay dailyData={forecastData[0]} />
         </div>
       </div>
     );
   } else {
     let apiKey = '14c3a03f2d1f08f51dfob8945097tf22';
-    let city = props.forecast.city;
 
-    let url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    let city = props.forecast.city;
+    let url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
 
     axios.get(url).then(handleForecast);
 
---
+++ src/ForecastDay.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function ForecastDay(props) {
+  function maxTemp() {
+    let temp = Math.round(props.dailyData.temperature.maximum);
+    return `${temp}`;
+  }
+
+  function minTemp() {
+    let temp = Math.round(props.dailyData.temperature.minimum);
+    return `${temp}`;
+  }
+
+  function day() {
+    let date = new Date(props.dailyData.time * 1000);
+    let day = date.getDay();
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="col">
+        <div> {day()}</div>
+        <img
+          src={props.dailyData.condition.icon_url}
+          alt={props.dailyData.condition.description}
+        />
+      </div>
+      <div>
+        <span className="min">{maxTemp()}°</span>
+        <span className="max">
+          {'  '}
+          {minTemp()}°
+        </span>
+      </div>
+    </div>
+  );
+}
